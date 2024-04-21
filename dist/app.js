2c4aac2f94b788b1fa4f5bc12fb808b0f6480c74
sanketio <sanket.parmar11@gmail.com>
2024-04-21 16:09:41 +0530

    Add intro section

--- dist/app.js
+++ dist/app.js
@@ -1,28 +1,79 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
-var __webpack_exports__ = {};
+/******/ 	var __webpack_modules__ = ({
 
+/***/ "./src/scss/styles.scss":
+/*!******************************!*\
+  !*** ./src/scss/styles.scss ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-const d = new Date();
-const hours = d.getHours();
-const night = hours >= 19 || hours <= 7; // between 7pm and 7am
-const body = document.querySelector('body');
-const toggle = document.getElementById('toggle');
-const input = document.getElementById('switch');
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://sanketio.github.io/./src/scss/styles.scss?");
 
-if (night) {
-	input.checked = true;
-	body.classList.add('night');
-}
+/***/ }),
 
-toggle.addEventListener('click', function() {
-	const isChecked = input.checked;
-	if (isChecked) {
-		body.classList.remove('night');
-	} else {
-		body.classList.add('night');
-	}
-});
+/***/ "./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/styles.scss */ \"./src/scss/styles.scss\");\n\n\nconst d = new Date();\nconst hours = d.getHours();\nconst night = hours >= 19 || hours <= 7; // between 7pm and 7am\nconst body = document.querySelector('body');\nconst toggle = document.getElementById('toggle');\nconst input = document.getElementById('switch');\n\nif (night) {\n\tinput.checked = true;\n\tbody.classList.add('night');\n}\n\ntoggle.addEventListener('click', function() {\n\tconst isChecked = input.checked;\n\tif (isChecked) {\n\t\tbody.classList.remove('night');\n\t} else {\n\t\tbody.classList.add('night');\n\t}\n});\n\nconst hand = document.querySelector('.emoji.wave-hand');\n\nfunction waveOnLoad() {\n\thand.classList.add('wave');\n\tsetTimeout(function() {\n\t\thand.classList.remove('wave');\n\t}, 2000);\n}\n\nsetTimeout(function() {\n\twaveOnLoad();\n}, 1000);\n\nhand.addEventListener('mouseover', function() {\n\thand.classList.add('wave');\n});\n\nhand.addEventListener('mouseout', function() {\n\thand.classList.remove('wave');\n});\n\n\n//# sourceURL=webpack://sanketio.github.io/./src/js/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/js/main.js");
+/******/ 	
 /******/ })()
 ;
--- src/js/main.js
+++ src/js/main.js
@@ -20,3 +20,24 @@ toggle.addEventListener('click', function() {
 		body.classList.add('night');
 	}
 });
+
+const hand = document.querySelector('.emoji.wave-hand');
+
+function waveOnLoad() {
+	hand.classList.add('wave');
+	setTimeout(function() {
+		hand.classList.remove('wave');
+	}, 2000);
+}
+
+setTimeout(function() {
+	waveOnLoad();
+}, 1000);
+
+hand.addEventListener('mouseover', function() {
+	hand.classList.add('wave');
+});
+
+hand.addEventListener('mouseout', function() {
+	hand.classList.remove('wave');
+});
